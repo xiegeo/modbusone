a0170336625fb8f6ccd4c1e633694ed8efc8c915
George Xie <george@xiegeo.com>
2016-10-10 01:49:10 -0700

    refactoring

--- serial_rtu.go
+++ serial_rtu.go
@@ -2,12 +2,21 @@ package modbusone
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/xiegeo/modbusone/crc"
 )
 
 //Modbus RTU Application Data Unit
 type RTU []byte
+
+func MakeRTU(p PDU) RTU {
+	return RTU(crc.Append(p))
+}
+
+func (r RTU) IsMulticast() bool {
+	return len(r) > 0 && r[0] == 0
+}
 
 func (r RTU) GetPDU() (PDU, error) {
 	if len(r) < 4 {
@@ -20,6 +29,13 @@ func (r RTU) GetPDU() (PDU, error) {
 	return p, nil
 }
 
-func MakeRTU(p PDU) RTU {
-	return RTU(crc.Append(p))
+//the minum Delay of 3.5 chars between packets
+func RTUMinDelay(s SerialPort) time.Duration {
+	delay := 1750 * time.Microsecond
+	br := time.Duration(s.BaudRate)
+	if br <= 19200 {
+		oneBitDuration := (time.Second + br - 1) / br //time it takes to send a bit
+		delay = (oneBitDuration*11*7 + 1) / 2         //time it takes to send 3.5 chars (a char of 8 bits takes 11 bits on wire)
+	}
+	return delay
 }
--- server_rtu.go
+++ server_rtu.go
@@ -18,25 +18,20 @@ const MaxRTUSize = 256
 //Serve runs the server and only returns after unrecoverable error, such as com is closed.
 //Right now Read is assumed to only read full packets, as per RTU delay based spec.
 func (s *RTUServer) Serve(handler ProtocalHandler) error {
-	delay := 1750 * time.Microsecond
-	if s.com.BaudRate <= 19200 {
-		br := time.Duration(s.com.BaudRate)
-		oneBitDuration := (time.Second + br - 1) / br //time it takes to send a bit
-		delay = (oneBitDuration*11*7 + 1) / 2         //time it takes to send 3.5 chars (a char of 8 takes 11 bits on wire)
-	}
+	delay := RTUMinDelay(s.com)
 
 	rb := make([]byte, MaxRTUSize)
 	var p PDU
 
-	wp := func(pdu PDU) error {
-		time.Sleep(delay)
-		_, err := s.com.Write(MakeRTU(pdu))
-		return err
-	}
-	wec := func(ec ExceptionCode) error {
-		return wp(ErrorReplyPacket(p, ec))
-	}
 	var ioerr error //make continue do io error checking
+	wp := func(pdu PDU) {
+		time.Sleep(delay)
+		_, ioerr = s.com.Write(MakeRTU(pdu))
+	}
+	wec := func(ec ExceptionCode) {
+		wp(ErrorReplyPacket(p, ec))
+	}
+
 	for ioerr == nil {
 		var n int
 		n, ioerr = s.com.Read(rb)
@@ -57,30 +52,29 @@ func (s *RTUServer) Serve(handler ProtocalHandler) error {
 		ec := p.Validate()
 		if ec != EcOK {
 			debugf("RTUServer auto return for error code:%v\n", ec)
-			ioerr = wec(ec)
+			wec(ec)
 			continue
 		}
 		fc := p.GetFunctionCode()
+		var out PDU
 		if fc.ReadToServer() {
-			var out PDU
 			out, err = handler.OnOutput(p)
 			if err != nil {
 				debugf("RTUServer handler.OnOutput error:%v\n", err)
-				ioerr = wec(EcServerDeviceFailure)
+				wec(EcServerDeviceFailure)
 				continue
 			}
-			ioerr = wp(out)
-			continue
-		}
-		if fc.WriteToServer() {
+		} else if fc.WriteToServer() {
 			err = handler.OnInput(p)
 			if err != nil {
 				debugf("RTUServer handler.OnInput error:%v\n", err)
-				ioerr = wec(EcServerDeviceFailure)
+				wec(EcServerDeviceFailure)
 				continue
 			}
-			ioerr = wp(p.RepToWrite())
-			continue
+			out = p.RepToWrite()
+		}
+		if !r.IsMulticast() {
+			wp(out)
 		}
 	}
 	return ioerr
